74a803d97bf78d266ae864a1d8bcf45dca479b91
blazerpaul15 <blazerpaul15@gmail.com>
2025-04-01 22:26:32 +0900

    sm8350-common: Build vendor.qti.hardware.bluetooth_audio to vendor

--- common.mk
+++ common.mk
@@ -96,7 +96,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth.audio-impl \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
